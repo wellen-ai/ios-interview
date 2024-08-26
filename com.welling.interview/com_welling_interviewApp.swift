924b7b77407e6cfdcf7088e46cda6f803d91fc59
Irwin Billing <irwin.billing@gmail.com>
2024-08-26 14:47:59 -0700

    Initial Commit

---
+++ com.welling.interview/com_welling_interviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  com_welling_interviewApp.swift
+//  com.welling.interview
+//
+//  Created by Irwin Billing on 2024-08-26.
+//
+
+import SwiftUI
+
+@main
+struct com_welling_interviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
